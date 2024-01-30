c58aa3a46cc407095696f3e48cac3548b12be5ee
trub <marcusdiedrich1@icloud.com>
2024-01-30 16:26:48 -0700

    Add files via upload

---
+++ step1.py
@@ -0,0 +1,7 @@
+# Variables are an essential part of Python and any programming language. A variable is a name that references or points to an object. You can declare a variable by writing the variable name on the left side of the assignment operator `=` and specifying the value to assign to that variable on the right side of the assignment operator:
+
+# variable_name = value
+
+# Create a variable called number and assign the value 5 to your new variable.
+
+number = 5
